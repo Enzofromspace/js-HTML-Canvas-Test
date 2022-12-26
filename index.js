01deed382fcdbc12ad166ea03ac89ff76180a5f7
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-26 20:49:56 +0300

    Fixed bug on line 22

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 // Write Javascript code!
+
 const canvas = document.querySelector('#draw');
 
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-ctx.strokeStyle = 'yellow';
+ctx.strokeStyle = '#BADA55';
 // full list of options for line join and line cap available on MDN Docs
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
-ctx.lineWidth = 50;
+ctx.lineWidth = 25;
 
 let isDrawing = false;
 let lastX = 0;
@@ -18,7 +19,7 @@ let direction = true;
 
 function draw(e){
   if(!isDrawing) return; // stop func if no mouse down
-  ctx.strokeStyle = `hsl(${hue}, 100%. 50%)`;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
   ctx.beginPath();
   ctx.moveTo(lastX,lastY);
   ctx.lineTo(e.offsetX, e.offsetY);
@@ -27,8 +28,9 @@ function draw(e){
   [lastX,lastY] = [e.offsetX, e.offsetY];
   //update color HSL
   hue++;
+
   //set range to match max/min hue
-  if(hue >= 360){
+  if (hue >= 360) {
     hue = 0;
   }
   //increment line width
@@ -40,7 +42,7 @@ function draw(e){
   } else {
     ctx.lineWidth--;
   }
-  console.log(hue);
+  //console.log(hue);
 }
 
 canvas.addEventListener('mousedown', (e) => {
@@ -51,4 +53,3 @@ canvas.addEventListener('mousedown', (e) => {
 canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
-
