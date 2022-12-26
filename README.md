12c2dbe756a8758667c386ae015947800eee88f1
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-26 20:06:12 +0300

    Setup initial canvas parameters and functions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # js-HTML-Canvas-Test
 
+Another project from the 30 day JS challenge by Wes Bos
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-an1hhe)
--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-// Import stylesheets
-import './style.css';
-
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const canvas = document.querySelector('#draw');
+
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADASS';
+// full list of options for line join and line cap available on MDN Docs
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e){
+  console.log(e);
+}
+
+canvas.addEventListener('mousemove', draw);
